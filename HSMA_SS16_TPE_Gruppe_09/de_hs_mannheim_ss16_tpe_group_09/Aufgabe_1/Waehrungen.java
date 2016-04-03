deb017ada51bdb22964fa03540fdf6e2b8210344
SeMi <1523322@stud.hs-mannheim.de>
2016-04-03 13:00:36 +0200

    Anpassung auf neuen Ordner

---
+++ HSMA_SS16_TPE_Gruppe_09/de_hs_mannheim_ss16_tpe_group_09/Aufgabe_1/Waehrungen.java
@@ -0,0 +1,14 @@
+package Aufgabe_1;
+
+public class Waehrungen {
+
+	// Rechnen mit 4 Nachkommastellen (Zahl mal 10000) fuer genaues Ergebnis mit long
+	//Klassenvariablen
+	static Waehrung USD = new Waehrung("USD",  "$", 10000);
+	static Waehrung Euro = new Waehrung("Euro", "€", 12690);
+	static Waehrung Yen = new Waehrung("Yen", "¥", 91);
+	static Waehrung Rubel = new Waehrung("Rubel", "RUB", 255);
+	static Waehrung SchweizerFranken = new Waehrung("SchweizerFranken", "CHF", 10509);
+	
+
+}
