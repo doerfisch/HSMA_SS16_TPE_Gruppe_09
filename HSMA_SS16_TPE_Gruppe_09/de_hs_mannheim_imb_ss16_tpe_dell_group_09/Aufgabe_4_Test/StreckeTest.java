c1d4de1813ae65b86285a8bfc8a83bb2f940952e
Sebastian Schuler <sebbl.schuler@gmail.com>
2016-05-31 20:06:41 +0200

    Feinschliff Tests

--- HSMA_SS16_TPE_Gruppe_09/de_hs_mannheim_imb_ss16_tpe_dell_group_09/Aufgabe_4_Test/StreckeTest.java
+++ HSMA_SS16_TPE_Gruppe_09/de_hs_mannheim_imb_ss16_tpe_dell_group_09/Aufgabe_4_Test/StreckeTest.java
@@ -4,11 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class StreckeTest {
+import Aufgabe_4.Block;
+import Aufgabe_4.Strecke;
+import Aufgabe_4.Zug;
 
+public class StreckeTest {
+	Block b1 = new Block(0, 10);
+	Block b2 = new Block(10, 15);
+	Block b3 = new Block(15, 25);
+	Block b4 = new Block(25, 35);
+	Block b5 = new Block(35, 50);
+	Block b6 = new Block(50, 60);
+	Block b7 = new Block(60, 65);
+	Block b8 = new Block(65, 70);
+
+	Zug a = new Zug('A', 6, 5, b1);
+	Zug b = new Zug('B', 11, 15, b2);
+	Zug c = new Zug('C', 20, 5, b3);
+	Zug d = new Zug('D', 30, 10, b4);
+	Zug e = new Zug('E', 45, 6, b5);
+
+	Strecke s1 = new Strecke(70, new Block[] { b1, b2, b3, b4, b5, b6, b7, b8 }, new Zug[] { a, b, c, d, e });
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStrecke() {
+		assertEquals(Strecke.class, new Strecke(70, new Block[] { b1, b2, b3, b4, b5, b6, b7, b8 }, new Zug[] { a, b, c, d, e }).getClass());
+		assertEquals("_----A---_B---_----C----_----D----_---------E----_---------_----_-----", s1.toString());
 	}
 
 }
--- HSMA_SS16_TPE_Gruppe_09/de_hs_mannheim_imb_ss16_tpe_dell_group_09/Aufgabe_4_Test/ZugTest.java
+++ HSMA_SS16_TPE_Gruppe_09/de_hs_mannheim_imb_ss16_tpe_dell_group_09/Aufgabe_4_Test/ZugTest.java
@@ -4,11 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Aufgabe_4.Block;
+import Aufgabe_4.Zug;
+
 public class ZugTest {
 
+	Block b1 = new Block(0, 10);
+	Zug a = new Zug('A', 6, 5, b1);
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testZug() {
+		assertEquals(Zug.class, new Zug('C', 0, 10, b1).getClass());
+		assertEquals('A', a.getZugName());
+		assertEquals(6, a.getPosition());
+	}
+	
+	public void testRun() {
+		a.run();
+		assertTrue(a.isAlive());
+		assertFalse(a.isInterrupted());
+		a.interrupt();
+		assertTrue(a.isInterrupted());
+		assertFalse(a.isAlive());
 	}
 
 }
